985df8f20aa7384c7186fce82a81536fc0a33912
Shashank Tiwari <st@uno.ai>
2022-04-02 13:17:54 -0700

    Enable Jaeger tracing in microservices-demo
    enable jaeger configuration and install jaeger services on kubernetes

--- src/productcatalogservice/server.go
+++ src/productcatalogservice/server.go
@@ -36,7 +36,7 @@ import (
 	"contrib.go.opencensus.io/exporter/stackdriver"
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/sirupsen/logrus"
-	//  "go.opencensus.io/exporter/jaeger"
+	"go.opencensus.io/exporter/jaeger"
 	"go.opencensus.io/plugin/ocgrpc"
 	"go.opencensus.io/stats/view"
 	"go.opencensus.io/trace"
